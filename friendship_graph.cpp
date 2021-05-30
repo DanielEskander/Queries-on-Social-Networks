93f3689c1bdadd29a5e789c7a6a7c679ca799505
Jason Em <jasonem@cs.ucsb.edu>
2021-05-29 19:44:01 -0700

    updated addFriendship

--- friendship_graph.cpp
+++ friendship_graph.cpp
@@ -7,44 +7,44 @@
 using namespace std;
 
 void rbTree::addFriend(string name1, string name2){
-    Node *first = new Node(name1, NULL);
-    Node *second = new Node(name2, NULL);
     rNode *add1 = findHelper(name1);
     rNode *add2 = findHelper(name2);
     if(add1 == NULL || add2 == NULL)
         return;
     if(add1->friends == NULL)
+        Node *second = new Node(name2, NULL);
         add1->friends = second;
     else
     {
         Node *lastNode1 = add1->friends;
         while(lastNode1->next != NULL)
         {
-            if (strcmp(lastNode1->name.c_str(), second->name.c_str()) == 0){
-                second = lastNode1->next;
+            if (strcmp(lastNode1->name.c_str(), name2.c_str()) == 0){
                 break;
             }
             lastNode1 = lastNode1->next;
         }
-        lastNode1->next = second;
-    }
+        if(lastNode1->next == NULL){
+            Node *second = new Node(name2, NULL);
+            lastNode1->next = second;
+        }
     if(add2->friends == NULL)
+        Node *first = new Node(name1, NULL);
         add2->friends = first;  
     else
     {
         Node *lastNode2 = add2->friends;
         while(lastNode2->next != NULL)
         {
-            if (strcmp(lastNode2->name.c_str(), first->name.c_str()) == 0){
-                first = lastNode2->next;
+            if (strcmp(lastNode2->name.c_str(), name2.c_str()) == 0){
                 break;
             }
             lastNode2 = lastNode2->next;
         }
-        lastNode2->next = first;
+        if(lastNode2->next == NULL){
+            Node *first = new Node(name1, NULL);
+            lastNode2->next = first;
+        }
     }
 }
-
-
-
 
